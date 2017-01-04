93504045852ad22bf5b9ff8dee32bf7fd5b25d21
Nicholas Westlake <n.westlake@bath.ac.uk>
2017-01-04 14:59:48 +0000

    Change key bindings
    - Add F3 and F4 for macros
    - Remove M-b for yank
    - Remove M-y which is used by Ergoemacs

--- lisp/nw/bindings.el
+++ lisp/nw/bindings.el
@@ -2,17 +2,16 @@
 (global-set-key [(meta right)] 'next-buffer)
 (global-set-key [(meta left)] 'previous-buffer)
 
+(global-set-key (kbd "<f3>") 'kmacro-start-macro-or-insert-counter)
+(global-set-key (kbd "<f4>") 'kmacro-end-or-call-macro)
 (global-set-key (kbd "<f5>") 'recompile)
 (global-set-key (kbd "<f7>") 'next-error)
 (global-set-key (kbd "<f6>") 'previous-error)
 (global-set-key (kbd "C-l") 'goto-line)
-(global-set-key (kbd "M-b") 'yank)
 
 (global-set-key (kbd "C-k") 'flyspell-auto-correct-word)
 
 (global-set-key (kbd "C-e") 'shell)
-
-(global-set-key (kbd "M-y") 'back-to-indentation)
 
 (global-set-key (kbd "C-t") 'pop-global-mark)
 
