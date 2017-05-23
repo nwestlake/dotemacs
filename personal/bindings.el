c03af4cf84b6f80c2213370ac77571c6083e6ba8
Nick Westlake <n.westlake@bath.ac.uk>
2017-05-23 16:55:05 +0100

    Remove non working keyboard shortcuts

--- personal/bindings.el
+++ personal/bindings.el
@@ -2,18 +2,17 @@
 (global-set-key [(meta right)] 'next-buffer)
 (global-set-key [(meta left)] 'previous-buffer)
 
-(global-set-key (kbd "<f3>") 'kmacro-start-macro-or-insert-counter)
-(global-set-key (kbd "<f4>") 'kmacro-end-or-call-macro)
-(global-set-key (kbd "<f5>") 'recompile)
-(global-set-key (kbd "<f7>") 'next-error)
-(global-set-key (kbd "<f6>") 'previous-error)
-(global-set-key (kbd "C-l") 'goto-line)
+;(global-set-key (kbd "<f3>") 'kmacro-start-macro-or-insert-counter)
+;(global-set-key (kbd "<f4>") 'kmacro-end-or-call-macro)
+;(global-set-key (kbd "<f5>") 'recompile)
+;(global-set-key (kbd "<f7>") 'next-error)
+;(global-set-key (kbd "<f6>") 'previous-error)
 
-(global-set-key (kbd "C-k") 'flyspell-auto-correct-word)
+;(global-set-key (kbd "C-k") 'flyspell-auto-correct-word)
 
 (global-set-key (kbd "C-e") 'shell)
 
-(global-set-key (kbd "C-t") 'pop-global-mark)
+;(global-set-key (kbd "C-t") 'pop-global-mark)
 
 (global-set-key (kbd "M-m") 'jump-char-forward)
 (global-set-key (kbd "M-M") 'jump-char-backward)
